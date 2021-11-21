01a2b88b9151abbecedc70f8bc73d4a4de484c39
Gao Kang <gaokang@Gaos-MacBook-Pro.local>
2021-11-21 13:12:03 +1300

    setup a interface for Physical and digital products

---
+++ DemoLibrary/DigitalProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DemoLibrary
+{
+    public class DigitalProductModel : IProductModel
+    {
+        public string Title { get; set; }
+        public bool HasOrderBeenCompleted { get; private set; }
+        public int TheNumhasbeenSent { get; private set; } = 5;
+
+        public void ShipItem(CustomerModel customer)
+        {
+            if (TheNumhasbeenSent <1)
+            {
+                TheNumhasbeenSent = 0;
+            }
+            else
+            {
+                Console.WriteLine($"{Title} has been sent to {customer.EmailAddress}.");
+                TheNumhasbeenSent -= 1;
+            }
+        }
+    }
+}
